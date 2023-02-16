284f05da89a35d017866dfa5a47761c09b9ade4a
Maksim <khitrov.maks@gmail.com>
2023-02-16 21:42:48 +0400

    Added logic

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const TelegramApi = require('node-telegram-bot-api');
-const { KHITROV_HOLIDAY_TOKEN: token, listOfCommands, flags } = require('./lib/data');
+const {
+  ABSTRACT_API_KEY,
+  KHITROV_HOLIDAY_TOKEN: token,
+  listOfCommands,
+  flags,
+} = require('./lib/data');
 const logger = require('./logger');
 const Commands = require('./lib/commands');
+const abstractApiUrlBuilder = require('./lib/abstractApiUrlBuilder');
+const getCurrentDate = require('./lib/getCurrentDate');
+const httpsGetJson = require('./lib/httpsGetJson');
+const abstractApiResponseParser = require('./lib/abstractApiResponseParser');
+const serializerArrayOfObjects = require('./lib/serializerArrayOfObjects');
 
 const bot = new TelegramApi(token, { polling: true });
-
 const commands = new Commands(listOfCommands);
 
 const start = () => {
@@ -22,9 +31,7 @@ const start = () => {
 
     logger.info(`Request's string: ${text}; Username: ${username}; User id: ${chatId}`);
 
-    const methodsDict = {
-      '/start': 'Please choose a flag!',
-    };
+    const methodsDict = { '/start': 'Please choose a flag!' };
     const message = methodsDict[text] || `"${text}" is not a command. Please try again!`;
 
     return bot.sendMessage(chatId, message, flags);
@@ -34,7 +41,27 @@ const start = () => {
     const { data, message } = msg;
     const { chat } = message;
     const chatId = chat.id;
+
     logger.info(`Callback query data: ${data}; User id: ${chatId}`);
+
+    const { year, month, day } = getCurrentDate();
+    const url = abstractApiUrlBuilder(ABSTRACT_API_KEY, data, year, month, day);
+    const json = await httpsGetJson(url).catch((error) => {
+      logger.error(`Error occurred while sending request. ${error}`);
+    });
+    const parsedJson = abstractApiResponseParser(json);
+
+    if (parsedJson.error) {
+      logger.error(`Callback query data error: ${parsedJson.error}`);
+      return bot.sendMessage(chatId, 'Response error');
+    }
+
+    if (parsedJson.amountOfHolidays === 0) {
+      logger.info('Callback query data: There is no holiday today');
+      return bot.sendMessage(chatId, 'There is no holiday today');
+    }
+
+    return bot.sendMessage(chatId, serializerArrayOfObjects(parsedJson.holidays));
   });
 };
 
